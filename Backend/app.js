72c4953135cada1c5de11cc52b05082c40564a80
DrBidoof <alwyn.lynch23@gmail.com>
2024-11-20 01:02:18 -0500

    Update app.js
    file name changed

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ import multer from "multer";
 
 /* Configuratuons */
 
-const __filename =fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
