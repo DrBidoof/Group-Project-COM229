ac3ff6400667464963d62bd5e7e77f4b490e77c7
DrBidoof <alwyn.lynch23@gmail.com>
2024-11-23 21:46:49 -0500

    getUserFriends function
    getUserFriends started but not tested as yet

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -1,23 +1,32 @@
 import { MongoClient } from "mongodb";
 
-export const getUser = async (req, res) => {
+export const getUser = async (req,res,client,dbName) => {
     try{
 
     } catch(err){
-
+        res.status(404).json({ message: err.message });
     }
 };
 
-export const getUserFriends = async (req, res) => {
+export const getUserFriends = async (req,res,client,dbName) => {
     try{
+        const { id } = req.params.id;
+        const db = client.db(dbName);
+        const userCollection = db.collection("Users");
+        const currentUser = await userCollection.findOne({ _id: id });
+        if (!currentUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const friends = user.friends; 
+        res.status(200).json(friends);
 
     } catch(err){
-
-    }
+        res.status(404).json({ message: err.message }); 
+    };
 };
 
 
-export const addRemoveFriend = async (req, res) => {
+export const addRemoveFriend = async (req, res,client,dbName) => {
     try{
 
     } catch(err){
