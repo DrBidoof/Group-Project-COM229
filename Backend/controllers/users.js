58c244d33b8f4ee56c43c9c7d4ba6ac93bd3f8ce
DrBidoof <alwyn.lynch23@gmail.com>
2024-11-23 22:34:45 -0500

    getUserFriends completed
    Need to test on postman still

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -8,21 +8,29 @@ export const getUser = async (req,res,client,dbName) => {
     }
 };
 
-export const getUserFriends = async (req,res,client,dbName) => {
-    try{
-        const { id } = req.params.id;
+export const getUserFriends = async (req, res, client, dbName) => {
+    try {
+        const { id } = req.params;  
         const db = client.db(dbName);
         const userCollection = db.collection("Users");
         const currentUser = await userCollection.findOne({ _id: id });
         if (!currentUser) {
             return res.status(404).json({ message: "User not found" });
         }
-        const friends = user.friends; 
-        res.status(200).json(friends);
 
-    } catch(err){
-        res.status(404).json({ message: err.message }); 
-    };
+        const friends = await Promise.all(currentUser.friends.map(async (_id) => {
+            const friend = await userCollection.findOne({ _id });
+            return friend;
+        }));
+        const formattedFriends = friends.map(({ _id, firstName, lastName, occupation, location, picturePath }) => {
+            return { _id, firstName, lastName, occupation, location, picturePath };
+        });
+
+        res.status(200).json(formattedFriends);
+
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
 };
 
 
@@ -30,6 +38,6 @@ export const addRemoveFriend = async (req, res,client,dbName) => {
     try{
 
     } catch(err){
-
+        res.status(404).json({ message: err.message });
     }
 };
