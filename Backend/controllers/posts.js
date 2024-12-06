5cfde82758a992af0fc7c74987195e3c15bcb7be
DrBidoof <alwyn.lynch23@gmail.com>
2024-12-06 00:00:44 -0500

    Update posts.js
    Made changes to posts.js
    
    Ready for testing

--- Backend/controllers/posts.js
+++ Backend/controllers/posts.js
@@ -1,41 +1,52 @@
+import { ObjectId } from "mongodb";
 
-/*Create*/
-
-export const createPost = async (req,res,client,dbName) => {
-    try{
+/* Create */
+export const createPost = async (req, res, client, dbName) => {
+    try {
         const post = {
-            userID : req.body.userID,
-            description : req.body.description,
-            picturePath : req.body.picturePath,
-        }
+            userID: req.body.userID,
+            description: req.body.description,
+            picturePath: req.body.picturePath,
+        };
+
         const db = client.db(dbName);
         const userCollection = db.collection("Users");
-        const currentUser = await userCollection.findOne({ _id: post.userID });
+
+        // Convert userID to ObjectId
+        const userIdObjectId = new ObjectId(post.userID);
+        const currentUser  = await userCollection.findOne({ _id: userIdObjectId });
+
+        if (!currentUser ) {
+            return res.status(404).json({ message: "User  not found" });
+        }
+
         const newPost = {
-            userID: currentUser._id,
-            firstName: currentUser.firstName,
-            lastName: currentUser.lastName,
-            location:  currentUser.location,
+            userID: currentUser ._id,
+            firstName: currentUser .firstName,
+            lastName: currentUser .lastName,
+            location: currentUser .location,
             description: post.description,
             picturePath: post.picturePath,
-            userPicturePath: currentUser.userPicturePath,
+            userPicturePath: currentUser .userPicturePath,
             likes: new Map([]), // Initialize as an empty map
-            comments: [], 
+            comments: [],
             createdAt: new Date(),
         };
+
         const postCollection = db.collection("Posts");
         const result = await postCollection.insertOne(newPost);
         const allPosts = await postCollection.find().toArray();
+
         res.status(201).json({
             message: "Post created successfully",
             newPostId: result.insertedId,
             allPosts,
         });
 
-    } catch (err){
-           res.status(409).json({ message: err.message }) 
+    } catch (err) {
+        res.status(409).json({ message: err.message });
     }
-}
+};
 
 export const getFeedPosts = async (req,res,client,dbName) =>{
     try{
@@ -43,8 +54,7 @@ export const getFeedPosts = async (req,res,client,dbName) =>{
         const postCollection = db.collection("Posts");
         const allPosts = await postCollection.find().toArray();
         res.status(201).json({
-            message: "Post created successfully",
-            newPostId: result.insertedId,
+            message: "Post retrieved successfully",
             allPosts,
         });
     } catch(err){
@@ -53,36 +63,40 @@ export const getFeedPosts = async (req,res,client,dbName) =>{
 }
 
 export const getUserPosts = async (req,res,client,dbName) => {
-    try{
-        const { id } = req.params; //id of the post
+    try {
+        const { id } = req.params; // id of the post
         const db = client.db(dbName);
         const postCollection = db.collection("Posts");
-        const userPosts = await postCollection.find({_id:id}).toArray();
+        
+        // Convert id to ObjectId
+        const userPosts = await postCollection.find({ _id: ObjectId(id) }).toArray();
+        
         res.status(200).json({
-            message: "Post created successfully",
+            message: "Posts retrieved successfully",
             userPosts,
         });
-    } catch(err){
-        res.status(404).json({ message: err.message }) ;
-    };
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
 }
 
 /*Update*/
 
-export const likePosts = async (req,res,client,dbName) => {
-    try{
-        const { id } = req.params;
-        const { userid } = req.params.userId;
+export const likePosts = async (req, res, client, dbName) => {
+    try {
+        const { id } = req.params; // id of the post
+        const { userId } = req.params; // userId from the request parameters
         const db = client.db(dbName);
         const postCollection = db.collection("Posts");
         
+        // Update the likes for the post
         const updatedPost = await postCollection.findOneAndUpdate(
             { _id: ObjectId(id) }, // Find post by ID
             {
                 $set: {
-                    [`likes.${userid}`]: {
+                    [`likes.${userId}`]: {
                         $cond: [
-                            { $eq: [`$likes.${userid}`, true] },
+                            { $eq: [`$likes.${userId}`, true] },
                             false,
                             true,
                         ],
@@ -100,7 +114,7 @@ export const likePosts = async (req,res,client,dbName) => {
             message: "Post updated successfully",
             updatedPost: updatedPost.value,
         });
-    } catch(err){
-        res.status(404).json({ message: err.message }) ;
-    };
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
 }
