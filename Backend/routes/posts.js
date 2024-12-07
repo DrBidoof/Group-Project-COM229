7b1a1d1b278dc67f603b05ebfb748b1c33507dce
DrBidoof <alwyn.lynch23@gmail.com>
2024-12-06 20:03:39 -0500

    All routes Working !!!

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 /*READ*/
 export default (client, dbName) => {
-        router.get("/", verifyToken, (req, res) => getFeedPosts(req, res, client, dbName));
-        router.get("/:userId/posts", verifyToken, (req, res) => getUserPosts(req, res, client, dbName));
+        router.get("/", verifyToken, (req, res) => getFeedPosts(req, res, client, dbName)); //get all the current users post
+        router.get("/:userId/posts", verifyToken, (req, res) => getUserPosts(req, res, client, dbName)); //get all the post of another user
         router.patch("/:id/like", verifyToken, (req, res) => likePosts(req, res, client, dbName));
 
         return router;
