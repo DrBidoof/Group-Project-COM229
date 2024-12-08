6c80b619902e2fff4e30832e67b5417067f4cd9d
DhruvSandhar <dhruvsandhar13@gmail.com>
2024-12-08 00:46:59 -0500

    updated

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,13 @@ app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended:true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}));
-app.use(cors());
+app.use(
+    cors({
+      origin: "https://com229-frontend-y849.onrender.com/", // Replace with your frontend URL
+      methods: ["GET", "POST", "PATCH", "DELETE"],
+      credentials: true,
+    })
+  );
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*file storage */
